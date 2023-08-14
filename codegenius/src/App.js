b7de74e76fdd4eb5599aacb9233cb7fae7ce5bc2
MarianaNamie <mariana.ribeiro@sptech.school>
2023-08-14 20:49:59 -0300

    Início do modal de logout

---
+++ codegenius/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ModalLogoutConfirmationLogout from './components/ModalConfirmationLogout/ModalConfirmationLogout';
+
+function App() {
+  return (
+    <div className="App">
+      <ModalLogoutConfirmationLogout/>
+    </div>
+  );
+}
+
+export default App;
