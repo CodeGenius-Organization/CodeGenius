53d9b399c97684fd5dea0ef6a39b295e5a04be1f
MarianaNamie <mariana.ribeiro@sptech.school>
2023-11-02 21:05:40 -0300

    feature:
    wip

--- codegenius/src/pages/Home.js
+++ codegenius/src/pages/Home.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import NavBar from '../components/NavBar';
 import ModaltoForm from '../components/ModalToForm';
-import './Home.css'
+import './Home.css';
+import home_img from '../img/Computer1.png';
 
 
 function Home() {
@@ -22,12 +23,32 @@ function Home() {
             <NavBar
                 onLoginModal={handleVisible}
             />
+            <div className='background'>
+                <div className='gradient'>
+                    <div className='container'>
+                        <div className='content'>
+                            <div className='home'>
+                                <div className='left-side'>
+                                    <h1>Explore as tendências tecnológicas atuais!</h1>
+                                    <p>Expanda seus conhecimentos sobre as tecnologias e ferramentas que estão bombando em TI de maneira simples e prática.</p>
+                                    <div className='bt-home'>
+                                        <button className='cadastre-se'>Cadastre-se</button>
+                                        <button className='saiba-mais'>Saiba mais</button>
+                                    </div>
+                                </div>
+                                <div className='right-side'>
+                                    <img className='home-img' src={home_img}/>
+                                </div>
+                            </div>
+                            <div className='sobre-nos'></div>
+                            <div className='codegenius'></div>
+                            <div className='equipe'></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-            <div className='bg1'></div>
-            <div className='bg2'></div>
-            <div className='bg1'></div>
-            <div className='bg2'></div>
-        
+
             <ModaltoForm
                 toggleModal={handleVisible}
                 visible={modalVisible}
