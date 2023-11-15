5ebf02682c1514484f82a587c6e7e172b747068e
PaulaMariaPires <paula.pires@sptech.school>
2023-11-15 14:08:29 -0300

    feat: formulário de contato

--- codegenius/src/routes/AppRoutes.js
+++ codegenius/src/routes/AppRoutes.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import LogOut from '../pages/LogOut'
+import Profile from '../components/student-profile/Profile'
+import CardLesson from '../components/CardLesson'
+import FormContact from '../components/FormContact'
 
 function AppRoutes() {
     return (
@@ -9,6 +12,9 @@ function AppRoutes() {
             <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/logout' element={<LogOut/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/cardlesson' element={<CardLesson/>}/>
+            <Route path='/formcontact' element={<FormContact/>}/>
             </Routes>
         </BrowserRouter>
     )
