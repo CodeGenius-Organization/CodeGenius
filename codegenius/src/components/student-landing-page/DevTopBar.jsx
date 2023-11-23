61bd988ab49729b7189eb46579f605ade97b3188
10340314 <victor.diaz@sptech.school>
2023-11-23 19:47:32 -0300

    feat: conexão api na landing page para buscar cursos de acordo com a categoria selecionada (ONGOING)

--- codegenius/src/components/student-landing-page/DevTopBar.jsx
+++ codegenius/src/components/student-landing-page/DevTopBar.jsx
@@ -8,29 +8,29 @@ import { FaCode } from "react-icons/fa6";
 
 import style from './DevTopBar.module.css'
 
-function TopBar() {
+function DevTopBar({ changeTab, currentTab} ) {
     const iconStyle = {color: "#FFF", width: "24px", height: "24px"}
     
     return (
         <>
             <div className={ style.container }>
-                <div className={ style.item }>
+                <div className={ `${style.item} ${currentTab === 'Desenvolvedor' ? style.active : ''}` } onClick={() => changeTab("Desenvolvedor")}>
                     <MdCode style={ iconStyle } />
                     <span>Desenvolvedor</span>
                 </div>
-                <div className={ style.item }>
+                <div className={ `${style.item} ${currentTab === 'Web' ? style.active : ''}` } onClick={() => changeTab("Web")}>
                     <FaLaptopCode style={ iconStyle } />
                     <span>Web</span>
                 </div>
-                <div className={ style.item }>
+                <div className={ `${style.item} ${currentTab === 'Mobile' ? style.active : ''}` } onClick={() => changeTab("Mobile")}>
                     <MdDeveloperMode style={ iconStyle } />
                     <span>Mobile</span>
                 </div>
-                <div className={ style.item }>
+                <div className={ `${style.item} ${currentTab === 'Front-end' ? style.active : ''}` } onClick={() => changeTab("Front-end")}>
                     <MdWeb style={ iconStyle } />
                     <span>Front-end</span>
                 </div>
-                <div className={ style.item }>
+                <div className={ `${style.item} ${currentTab === 'Back-end' ? style.active : ''}` } onClick={() => changeTab("Back-end")}>
                     <FaCode style={ iconStyle } />
                     <span>Back-end</span>
                 </div>
@@ -39,4 +39,4 @@ function TopBar() {
     )
 }
 
-export default TopBar
+export default DevTopBar
--- codegenius/src/pages/student/landing-page/LandingPage.jsx
+++ codegenius/src/pages/student/landing-page/LandingPage.jsx
@@ -1,38 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from '../../../Api'
 
 import style from './LandingPage.module.css'
 
 import DevTopBar from '../../../components/student-landing-page/DevTopBar'
 import Filters from '../../../components/student-landing-page/Filters'
-import CardLesson from '../../../components/card-lesson/CardLesson'
 
 function LandingPage() {
+    // const [selectedCategory, setSelectedCategory] = useState('Desenvolvedor');
+    // const [coursesCache, setCoursesCache] = useState({});
+    // const [courses, setCourse] = useState([])
+
+    useEffect(() => {
+        api.get(`course/courses/category/Desenvolvimento`, 
+        {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                console.log("okay")
+            } else {
+                throw new Error("Ocorreu um erro interno")
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    })
+
     return (
+        
         <>
             <div className={ style.main_section }>
                 <DevTopBar />
                 <Filters />
                 <div className={ style.course_list }>
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
-                    <CardLesson />
+
                 </div>
             </div>
         </>
