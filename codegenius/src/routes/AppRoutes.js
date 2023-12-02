22c1b78a3aaf41fb775954ae0164cd62c8ac4d3c
MarianaNamie <mariana.ribeiro@sptech.school>
2023-12-01 23:27:18 -0300

    WIP

--- codegenius/src/routes/AppRoutes.js
+++ codegenius/src/routes/AppRoutes.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import Institutional from '../pages/institutional/Institutional'
 import FormContact from '../pages/form-contact/FormContact'
+import CourseCreation from '../pages/teacher/CourseCreation'
+import ModuleCreation from '../pages/teacher/ModuleCreation'
+import HomeTeacher from '../pages/home/HomeTeacher'
 
 function AppRoutes() {
     return (
@@ -11,6 +14,9 @@ function AppRoutes() {
                 <Route path='/' element={<Home/>} />
                 <Route path='/institutional' element={<Institutional/>}/>
                 <Route path='/form-contact' element={<FormContact/>}/>
+                <Route path='/prof' element={<CourseCreation/>}/>
+                <Route path='/prof2' element={<ModuleCreation/>}/>
+                <Route path='/prof3' element={<HomeTeacher/>}/>
             </Routes>
         </BrowserRouter>
     )
